eb32c2ebda37998f06ecaecadfd25d53825019db
Yosep Setiawan <yosep.setiawan@yahoo.co.id>
2020-11-06 14:12:20 +0700

    Add MVC to application

--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,21 +27,11 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
-            //This is a custom middleware
-            //app.Use(async (context, next) =>
-            //{
-            //    await context.Response.WriteAsync("This is a custom middleware!\n");
-            //    await next();
-            //});
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
